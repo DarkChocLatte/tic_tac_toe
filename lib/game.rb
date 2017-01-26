3e23a73fbdefa5210e61fe5a58257d3a6ba8107f
DarkChocLatte <darkchoclatte@gmail.com>
2017-01-26 13:19:33 +0000

    test added: tested that game initializes with turn set to 'player1's turn'

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,11 @@
 class Game
   attr_reader :player1, :player2
+  attr_accessor :turn
 
   def initialize(player_klass1, player_klass2)
     @player1 = player_klass1.new
     @player2 = player_klass2.new
+    @turn    = "#{@player1.name}'s turn"
   end
 
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -10,11 +10,17 @@ describe Game do
   let(:player2){ double( :player)}
 
   before do
+    allow(player1).to receive(:name).and_return("player who must not be named")
     @game = Game.new(player1_class, player2_class)
   end
 
   it "A new instance of Game is intiialized with 2 players" do
     expect(@game.player1).to eq(player1)
     expect(@game.player2).to eq(player2)
+  end
+
+  it "A new instance of Game is intiated with the turn set to 'player 1's turn'" do
+    allow(player1).to receive(:name).and_return("player who must not be named")
+    expect(@game.turn).to eq("player who must not be named's turn")
   end
 end
