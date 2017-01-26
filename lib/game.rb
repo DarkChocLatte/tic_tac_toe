6b360ea4a70280a1c12538ab82df633fd50728a4
DarkChocLatte <darkchoclatte@gmail.com>
2017-01-26 13:52:16 +0000

    test added: Game initializes with an instance of board

--- lib/game.rb
+++ lib/game.rb
@@ -1,15 +1,13 @@
 class Game
-  attr_reader :player1, :player2
+  attr_reader :player1, :player2, :board
   attr_accessor :turn
 
-  def initialize(player_klass1, player_klass2)
+  def initialize(player_klass1, player_klass2, board_klass, cell_klass)
     @player1 = player_klass1.new
     @player2 = player_klass2.new
     @turn    = "#{@player1.name}'s turn"
+    @board   = board_klass.new(cell_klass.new)
   end
-
-
-
 
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,9 +9,15 @@ describe Game do
   let(:player2_class) { double( :Player, new: player2)}
   let(:player2){ double( :player)}
 
+  let(:cell_class) { double( :Cell, new: cell)}
+  let(:cell){ double( :cell)}
+
+  let(:board_class) { double( :Board, new: board)}
+  let(:board){ double( :board)}
+
   before do
     allow(player1).to receive(:name).and_return("player who must not be named")
-    @game = Game.new(player1_class, player2_class)
+    @game = Game.new(player1_class, player2_class, board_class,cell_class)
   end
 
   it "A new instance of Game is intiialized with 2 players" do
@@ -22,5 +28,9 @@ describe Game do
   it "A new instance of Game is intiated with the turn set to 'player 1's turn'" do
     allow(player1).to receive(:name).and_return("player who must not be named")
     expect(@game.turn).to eq("player who must not be named's turn")
+  end
+
+  it "A new instance of Game is initiated with a board" do
+    expect(@game.board).to eq(board)
   end
 end
