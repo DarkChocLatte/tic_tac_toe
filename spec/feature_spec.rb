034aa86f0af0f81cae2d8fa4c27253ec4d8d3e08
DarkChocLatte <darkchoclatte@gmail.com>
2017-01-27 12:25:26 +0000

    feature tested that a player is able to place a mark on the board

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -7,4 +7,8 @@ describe "When a game is created" do
   it "A player may see the board" do
     expect(@game.board.show).to be_an_instance_of(Array)
   end
+
+  it "A player may take a turn" do
+    expect(@game.board.board_place_mark(2,3,'X'))
+  end
 end
