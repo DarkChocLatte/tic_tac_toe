d946253cc65d2a68baa4b52588795fdf09607568
DarkChocLatte <darkchoclatte@gmail.com>
2017-01-27 11:23:13 +0000

    fixed bug that stopped cell from being created by game by removing .new from cell i game and allowing the instance to be created from within the new instance of board class

--- lib/board.rb
+++ lib/board.rb
@@ -6,6 +6,10 @@ class Board
     @show = Array.new(9){cell_Klass.new}
   end
 
+  def board_place_mark(column, row, input)
+    raise 'Cannot place mark there: already a mark there' if @show[column-1][row-1] != ''
+    @show[column-1][row-1] = input
+  end
 
 
 
--- lib/game.rb
+++ lib/game.rb
@@ -6,7 +6,7 @@ class Game
     @player1 = player_klass1.new
     @player2 = player_klass2.new
     @turn    = "#{@player1.name}'s turn"
-    @board   = board_klass.new(cell_klass.new)
+    @board   = board_klass.new(cell_klass)
   end
 
 
